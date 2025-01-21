47d197291e67c135256b7f19ed431103a8caa996
Jimin Ku <82087246+jimizip@users.noreply.github.com>
2025-01-21 09:55:43 +0900

    [Silver I] Title: RGB거리, Time: 56 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -0,0 +1,17 @@
+# 입력
+N = int(input())
+
+arr = [list(map(int, input().split())) for _ in range(N)]
+dp = [[0] * 3 for _ in range(N)]
+
+# 초기화
+dp[0] = arr[0]
+
+# 점화식
+for i in range(1, N):
+    dp[i][0] = min(dp[i-1][1], dp[i-1][2]) + arr[i][0]
+    dp[i][1] = min(dp[i-1][0], dp[i-1][2]) + arr[i][1]
+    dp[i][2] = min(dp[i-1][0], dp[i-1][1]) + arr[i][2]
+
+# 출력
+print(min(dp[N-1]))
