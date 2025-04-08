1221c45ac307d9ce9f72a50fc418637c0c85bbb9
Jimin Ku <82087246+jimizip@users.noreply.github.com>
2025-04-08 17:06:11 +0900

    [Silver V] Title: 다리 놓기, Time: 80 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.py
@@ -0,0 +1,19 @@
+T = int(input())
+
+for _ in range(T):
+    N, M = map(int, input().split())
+    dp = [[0] * (M + 1) for _ in range(N + 1)]
+    
+    for i in range(1, M+1):
+        dp[1][i] = i
+
+    for i in range(2, N+1):
+        for j in range(2, M+1):
+            if i > j:
+                continue
+            if i == j:
+                dp[i][j] = 1
+            else:
+                dp[i][j] = dp[i-1][j-1] + dp[i][j-1]
+    
+    print(dp[N][M])
