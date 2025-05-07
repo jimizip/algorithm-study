c215a1cad0c0e44bdb9a8c7f42df4ab4b94595c2
Jimin Ku <82087246+jimizip@users.noreply.github.com>
2025-05-07 17:40:53 +0900

    [Silver I] Title: 전쟁 - 전투, Time: 44 ms, Memory: 32424 KB -BaekjoonHub

--- 백준/Silver/1303. 전쟁 － 전투/전쟁 － 전투.py
+++ 백준/Silver/1303. 전쟁 － 전투/전쟁 － 전투.py
@@ -1,4 +1,6 @@
-from collections import deque
+import sys
+sys.setrecursionlimit(10000)
+
 N, M = map(int, input().split())
 graph = [list(input()) for _ in range(M)]
 w_cnt = 0
@@ -7,29 +9,22 @@ b_cnt = 0
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-def bfs(color, x, y):
-    cnt = 0
-    Q = deque()
-    Q.append((x, y))
+def dfs(color, x, y):
+    cnt = 1 # 현재 칸 포함
     graph[x][y] = 0
 
-    while Q:
-        x, y = Q.popleft()
-        cnt += 1
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0 <= nx < M and 0 <= ny < N and graph[nx][ny] == color:
-                Q.append((nx, ny))
-                graph[nx][ny] = 0
-    
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0 <= nx < M and 0 <= ny < N and graph[nx][ny] == color:
+            cnt += dfs(color, nx, ny)
     return cnt
 
 for i in range(M):
     for j in range(N):
         if graph[i][j] == 'W':
-            w_cnt += bfs('W', i, j) ** 2
+            w_cnt += dfs('W', i, j) ** 2
         elif graph[i][j] == 'B':
-            b_cnt += bfs('B', i, j) ** 2
+            b_cnt += dfs('B', i, j) ** 2
 
 print(w_cnt, b_cnt)
