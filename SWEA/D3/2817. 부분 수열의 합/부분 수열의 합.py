1e57957277225297b7d2470300a4a8b415ed1f0c
Jimin Ku <82087246+jimizip@users.noreply.github.com>
2025-05-19 20:41:46 +0900

    [D3] Title: 부분 수열의 합, Time: 498 ms, Memory: 73,472 KB -BaekjoonHub

--- SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.py
+++ SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.py
@@ -1,13 +1,18 @@
-from itertools import combinations
 T = int(input())
-for tc in range(1, T+1):
+
+def dfs(i, result):
+    global cnt
+    if result == K:
+        cnt += 1
+        return
+    if i == N:
+        return
+    dfs(i + 1, result + arr[i])
+    dfs(i+1, result)
+
+for tc in range(1, T + 1):
     N, K = map(int, input().split())
     arr = list(map(int, input().split()))
     cnt = 0
-
-    for i in range(1, N+1):
-        for j in combinations(arr, i):
-            if sum(j) == K:
-                cnt += 1
-    
+    dfs(0, 0)
     print(f'#{tc} {cnt}')
