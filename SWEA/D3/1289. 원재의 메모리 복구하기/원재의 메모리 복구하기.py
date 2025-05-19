f233364441edb8dfe9dfefc5e830280cd9b82e91
Jimin Ku <82087246+jimizip@users.noreply.github.com>
2025-05-19 18:44:58 +0900

    [D3] Title: 원재의 메모리 복구하기, Time: 65 ms, Memory: 58,368 KB -BaekjoonHub

--- SWEA/D3/1289. 원재의 메모리 복구하기/원재의 메모리 복구하기.py
+++ SWEA/D3/1289. 원재의 메모리 복구하기/원재의 메모리 복구하기.py
@@ -1,25 +1,11 @@
 T = int(input())
 for tc in range(1, T+1):
-    text = list(map(int, input()))
-    be = [0]*(len(text))
+    text = list(input())
+    be = ['0']*len(text)
     cnt = 0
-
-    while text:
-        if 1 in text:
-            idx_1 = text.index(1)
-            for i in range(idx_1, len(be)):
-                be[i] = 1
+    for i in range(len(be)):
+        if be[i] != text[i]:
+            be[i:] = text[i]*len(be[i:]) # 덮어쓰기
             cnt += 1
 
-        for i in range(len(text)):
-            if text[0] == be[0]:
-                text.pop(0)
-                be.pop(0)
-                
-        if 0 in text:
-            idx_0 = text.index(0)
-            for i in range(idx_0, len(be)):
-                be[i] = 0
-            cnt += 1
-    
     print(f'#{tc} {cnt}')
