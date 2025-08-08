736f71dd141a46c0ab6b7bb4de89923676670915
Jimin Ku <82087246+jimizip@users.noreply.github.com>
2025-08-08 20:29:33 +0900

    [Silver I] Title: 용돈 관리, Time: 2940 ms, Memory: 36264 KB -BaekjoonHub

--- 백준/Silver/6236. 용돈 관리/용돈 관리.py
+++ 백준/Silver/6236. 용돈 관리/용돈 관리.py
@@ -1,26 +1,29 @@
 N, M = map(int, input().split())
 arr = []
-
 for _ in range(N):
     arr.append(int(input()))
 
-start = max(arr)
-end = sum(arr)
+start = max(arr)  # 최소한 가장 큰 금액은 인출할 수 있어야 함
+end = sum(arr)    # 모든 금액을 한 번에 인출하는 경우
+
+answer = 0
 
 while start <= end:
     mid = (start + end) // 2
-    tmp = mid
+    
+    current_sum = 0
     cnt = 1
-
     for i in arr:
-        if tmp - i < 0 :
+        if current_sum + i > mid:
             cnt += 1
-            tmp = mid
-        tmp -= i
+            current_sum = i
+        else:
+            current_sum += i
     
     if cnt > M:
         start = mid + 1
     else:
+        answer = mid
         end = mid - 1
 
-print(mid)
+print(answer)
