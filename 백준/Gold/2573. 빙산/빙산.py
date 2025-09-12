7cbb860cd8f8ada76b7c935254bb58502a60f6df
Jimin Ku <82087246+jimizip@users.noreply.github.com>
2025-09-12 16:17:10 +0900

    [Gold IV] Title: 빙산, Time: 4568 ms, Memory: 35380 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.py
+++ 백준/Gold/2573. 빙산/빙산.py
@@ -1,65 +1,55 @@
-import sys
 from collections import deque
 
-input = sys.stdin.readline
-
-n, m = map(int, input().split())
-
-graph = []
-
-for _ in range(n):
-    graph.append(list(map(int, input().split())))
-
-visited = [[False] * m for _ in range(n)]
+# 입력
+N, M = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(N)]
 year = 0
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
 
-def bfs(x, y):
-    queue = deque()
-    queue.append((x, y))
-    visited[x][y] = True
-    
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
+def bfs(a, b):
+    q = deque()
+    q.append((a, b))
+    visited[a][b] = 1
 
-    while queue:
-        x, y = queue.popleft()
+    while q:
+        x, y = q.popleft()
 
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
 
-            if 0 <= nx < n and 0 <= ny < m:
+            if 0 <= nx < N and 0 <= ny < M and not visited[nx][ny]:
                 if graph[nx][ny] == 0:
-                    melt_num[x][y] += 1
-                
-                if not visited[nx][ny] and graph[nx][ny] != 0:
-                    queue.append((nx, ny))
-                    visited[nx][ny] = True
+                    melt[x][y] += 1
+                else:
+                    visited[nx][ny] = 1
+                    q.append((nx, ny))
 
 
 while True:
-    melt_num = [[0] * m for _ in range(n)]             
-    cnt = 0  
-    visited = [[False] * m for _ in range(n)]
+    visited = [[0] * M for _ in range(N)]
+    melt = [[0] * M for _ in range(N)]
+    count = 0
 
-    for i in range(n): # 한 덩이가 나눠졌는지 cnt로 기록
-        for j in range(m):
-            if graph[i][j] != 0 and not visited[i][j]:
+    for i in range(N):
+        for j in range(M):
+            if graph[i][j] > 0 and visited[i][j] == 0:
                 bfs(i, j)
-                cnt += 1
+                count += 1
 
-    for i in range(n): # 1 년 후에 빙하가 녹은 높이 반영
-        for j in range(m):
-            if melt_num[i][j]:
-                graph[i][j] -= melt_num[i][j]
+    for i in range(N):
+        for j in range(M):
+            if melt[i][j] > 0:  # 녹을 빙산에 대해서만 연산
+                graph[i][j] -= melt[i][j]
                 if graph[i][j] < 0:
                     graph[i][j] = 0
-    year += 1
 
-    if cnt == 0: # 나눠지지 않고 한 덩이가 바로 다 녹았다면 0 출력 후 종료
+    if count >= 2:
+        print(year)
+        break
+    elif count == 0:
         print(0)
         break
 
-    if cnt >= 2: # 두 덩이 이상으로 나눠졌다면 걸린 년수 출력 후 종료
-        print(year - 1)
-        break
+    year += 1
