f13921d17a91b66f73d616173c46d499c8888216
Jimin Ku <82087246+jimizip@users.noreply.github.com>
2025-11-12 01:04:50 +0900

    [D2] Title: 파스칼의 삼각형, Time: 63 ms, Memory: 51,328 KB -BaekjoonHub

--- SWEA/D2/2005. 파스칼의 삼각형/파스칼의 삼각형.py
+++ SWEA/D2/2005. 파스칼의 삼각형/파스칼의 삼각형.py
@@ -1,14 +1,13 @@
 T = int(input())
-for tc in range(1, T+1):
+for t in range(1, T+1):
     N = int(input())
-    d = [[0]*N for _ in range(N+1)]
-    print(f'#{tc}')
-    for i in range(1, N+1):
-        d[i][0] = 1
-    
-    for n in range(2, N+1):
-        for j in range(1, i):
-            d[n][j] = d[n-1][j-1] + d[n-1][j]
+    arr = [[0]*N for _ in range(N)]
+    arr[0][0] = 1
+    for i in range(1, N):
+        for j in range(N):
+            arr[i][j] = arr[i-1][j-1] + arr[i-1][j]
 
-    for i in range(1, N+1):
-        print(*d[i][:i])
+    print(f'#{t}')
+    result = ''
+    for i in range(N):
+        print(*(arr[i][:i+1]))
