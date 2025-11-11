7010549e82ed4535b71e74a2edced46b80025321
Jimin Ku <82087246+jimizip@users.noreply.github.com>
2025-11-12 00:02:15 +0900

    [D2] Title: 어디에 단어가 들어갈 수 있을까, Time: 70 ms, Memory: 52,480 KB -BaekjoonHub

--- SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.py
+++ SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.py
@@ -1,27 +1,31 @@
 T = int(input())
-for tc in range(1, T+1):
+for t in range(1, T+1):
     N, K = map(int, input().split())
-    board = [list(map(int, input().split())) for _ in range(N)]
+    arr = [list(map(int, input().split())) for _ in range(N)]
     cnt = 0
-    
-    for i in range(N):
-        tmp_row = 0
-        for j in range(N):
-            if board[i][j] == 1:
-                tmp_row += 1
-            if board[i][j] == 0 or j == N-1:
-                if tmp_row == K:
-                    cnt+=1
-                tmp_row = 0
+    tmp = 0
 
+    # 가로 체크
     for i in range(N):
-        tmp_col = 0
+        tmp = 0
         for j in range(N):
-            if board[j][i] == 1:
-                tmp_col += 1
-            if board[j][i] == 0 or j == N-1:
-                if tmp_col == K:
-                    cnt +=1   
-                tmp_col = 0     
-    
-    print(f'#{tc} {cnt}')
+            if arr[i][j] == 1:
+                tmp += 1
+            if arr[i][j] == 0 or j == N-1:
+                if tmp == K:
+                    cnt += 1
+                tmp = 0
+
+
+    # 세로 체크
+    for i in range(N):
+        tmp = 0
+        for j in range(N):
+            if arr[j][i] == 1:
+                tmp += 1
+            if arr[j][i] == 0 or j == N - 1:
+                if tmp == K:
+                    cnt += 1
+                tmp = 0
+
+    print(f'#{t} {cnt}')
