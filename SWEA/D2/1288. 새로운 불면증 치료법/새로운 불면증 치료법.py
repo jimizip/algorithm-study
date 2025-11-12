b36be244890aa672c6a607cd3ef7ff4f4b421e56
Jimin Ku <82087246+jimizip@users.noreply.github.com>
2025-11-13 02:45:16 +0900

    [D2] Title: 새로운 불면증 치료법, Time: 86 ms, Memory: 61,312 KB -BaekjoonHub

--- SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.py
+++ SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.py
@@ -1,13 +1,17 @@
 T = int(input())
-for tc in range(1, T+1):
+for t in range(1, T+1):
+    num = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     N = int(input())
-    set_list = set()
-    i = 1
-    t = 0
-    while len(set_list) != 10:
-        t= str(i*N)
-        for j in t:
-            set_list.add(int(j))
-        i += 1
-    
-    print(f'#{tc} {int(t)}')
+    K = 0
+    tmp = 0
+    while len(num) != 0:
+        K += 1
+        tmp = str(N*K)
+        if len(tmp) > 1:
+            for i in tmp:
+                if i in num:
+                    num.remove(i)
+        else:
+            if tmp in num:
+                num.remove(tmp)
+    print(f'#{t} {tmp}')
