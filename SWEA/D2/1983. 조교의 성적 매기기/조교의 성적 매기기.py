839f891f3a61d6bbe504567dcbe99cd34c3a52e9
Jimin Ku <82087246+jimizip@users.noreply.github.com>
2025-11-16 02:09:17 +0900

    [D2] Title: 조교의 성적 매기기, Time: 66 ms, Memory: 53,504 KB -BaekjoonHub

--- SWEA/D2/1983. 조교의 성적 매기기/조교의 성적 매기기.py
+++ SWEA/D2/1983. 조교의 성적 매기기/조교의 성적 매기기.py
@@ -1,15 +1,14 @@
 T = int(input())
-for tc in range(1, T+1):
+grade_table = ['A+', 'A0', 'A-', 'B+', 'B0', 'B-', 'C+', 'C0', 'C-', 'D0']
+for t in range(1, T+1):
     N, K = map(int, input().split())
-    sli = N // 10
-    score = ["A+", "A0", "A-", "B+", "B0", "B-", "C+", "C0", "C-", "D0"]
-    stu = []
-    stu_sort = []
-    for i in range(N):
-        mid, final, task = map(int, input().split())
-        stu.append((mid*0.35)+(final*0.45)+(task*0.2))
-    stu_sort = sorted(stu, reverse=True)
-    
-    tmp = (stu_sort.index((stu[K-1])))
-
-    print(f'#{tc} {score[tmp//sli]}')
+    stu_arr = []
+    for _ in range(N):
+        m, f, a = map(int, input().split())
+        avg = m*0.35 + f*0.45 + a*0.2
+        stu_arr.append(avg)
+    rank = sorted(stu_arr, reverse=True)
+    k_score = stu_arr[K-1]
+    k_index = rank.index(k_score)
+    grade_idx = k_index * 10 // N   # N명이면 10개로 나눔
+    print(f'#{t} {grade_table[grade_idx]}')
