bd5fe4a5c64bf5c68ae8875f992c7aab1d332cc7
Jimin Ku <82087246+jimizip@users.noreply.github.com>
2025-11-18 13:39:42 +0900

    [D2] Title: 간단한 소인수분해, Time: 60 ms, Memory: 52,992 KB -BaekjoonHub

--- SWEA/D2/1945. 간단한 소인수분해/간단한 소인수분해.py
+++ SWEA/D2/1945. 간단한 소인수분해/간단한 소인수분해.py
@@ -1,27 +1,32 @@
 T = int(input())
-for tc in range(1, T+1):
+for t in range(1, T+1):
     N = int(input())
-    a = 0
-    b = 0
-    c = 0
-    d = 0 
-    e = 0
+    result = []
+    two = 0
+    three = 0
+    five = 0
+    seven = 0
+    eleven = 0
 
-    while N != 1:
-        if N % 2 == 0:
-            a += 1
-            N /= 2
-        elif N % 3 == 0:
-            b += 1
-            N /= 3
-        elif N % 5 == 0:
-            c += 1
-            N /= 5
-        elif N % 7 == 0:
-            d += 1
-            N /= 7
-        elif N % 11 == 0:
-            e += 1
-            N /= 11
-    
-    print(f'#{tc} {a} {b} {c} {d} {e}')
+    while N % 2 == 0:
+        N /= 2
+        two += 1
+    result.append(two)
+    while N % 3 == 0:
+        N /= 3
+        three += 1
+    result.append(three)
+    while N % 5 == 0:
+        N /= 5
+        five += 1
+    result.append(five)
+    while N % 7 == 0:
+        N /= 7
+        seven += 1
+    result.append(seven)
+    while N % 11 == 0:
+        N /= 11
+        eleven += 1
+    result.append(eleven)
+
+    print(f'#{t}', *result)
