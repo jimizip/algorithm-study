5e1d888e5cb50452f258527f48c1527b6338ef58
Jimin Ku <82087246+jimizip@users.noreply.github.com>
2025-11-18 14:21:20 +0900

    [D2] Title: 가랏! RC카!, Time: 65 ms, Memory: 53,760 KB -BaekjoonHub

--- SWEA/D2/1940. 가랏！ RC카！/가랏！ RC카！.py
+++ SWEA/D2/1940. 가랏！ RC카！/가랏！ RC카！.py
@@ -1,16 +1,21 @@
 T = int(input())
-for tc in range(1, 1+T):
-    N = int(input())
-    result = 0
-    s = 0
-    for _ in range(N):
+for tc in range(1, T + 1):
+    n = int(input())
+
+    v = 0  # 현재 속도 (m/s)
+    dist = 0  # 총 이동거리 (m)
+
+    for _ in range(n):
         cmd = list(map(int, input().split()))
-        if cmd[0] == 1:
-            s += cmd[1]
-        elif cmd[0] == 2:
-            s -= cmd[1]
-            if s < 0:
-                s = 0
-        result += s
-    
-    print(f'#{tc} {result}')
+
+        if cmd[0] == 1:  # 가속
+            v += cmd[1]  # 속도 = 현재속도 + 가속도
+        elif cmd[0] == 2:  # 감속
+            v -= cmd[1]  # 속도 = 현재속도 - 가속도
+            if v < 0:  # 속도가 음수가 되면 0으로 설정
+                v = 0
+        # cmd[0] == 0이면 속도 유지 (아무것도 안함)
+
+        dist += v  # 1초 동안 현재 속도만큼 이동
+
+    print(f"#{tc} {dist}")
