2bb7a71cb2a7858d28481655e017014a121294e4
Jimin Ku <82087246+jimizip@users.noreply.github.com>
2025-11-18 15:50:09 +0900

    [D2] Title: 시각 덧셈, Time: 61 ms, Memory: 53,632 KB -BaekjoonHub

--- SWEA/D2/1976. 시각 덧셈/시각 덧셈.py
+++ SWEA/D2/1976. 시각 덧셈/시각 덧셈.py
@@ -1,14 +1,11 @@
 T = int(input())
-for tc in range(1, 1+T):
-    arr = list(map(int, input().split()))
-    h = arr[0] + arr[2]
-    m = arr[1] + arr[3]
-
-    if h > 12:
-        h -= 12
-    
-    if m > 59:
-        h += (m//60)
-        m = m - 60
-    
-    print(f'#{tc} {h} {m}')
+for tc in range(1, T + 1):
+    fh, fm, lh, lm = map(int, input().split())
+    rh = fh + lh
+    rm = fm + lm
+    if rm > 59:
+        rm -= 60
+        rh += 1
+    if rh > 12:
+        rh -= 12
+    print(f'#{tc} {rh} {rm}')
