29aeef0db9d72b015f7d003c05f88337298a06e5
Jimin Ku <82087246+jimizip@users.noreply.github.com>
2025-11-18 16:50:23 +0900

    [D2] Title: [파이썬 S/W 문제해결 기본] 1일차 - 숫자 카드, Time: 65 ms, Memory: 52,608 KB -BaekjoonHub

---
+++ SWEA/D2/4834. ［파이썬 S／W 문제해결 기본］ 1일차 － 숫자 카드/［파이썬 S／W 문제해결 기본］ 1일차 － 숫자 카드.py
@@ -0,0 +1,22 @@
+T = int(input())
+for tc in range(1, T + 1):
+    n = int(input())
+    cards = input()
+
+    cnt = {}
+
+    for c in cards:
+        if int(c) in cnt:
+            cnt[int(c)] += 1
+        else:
+            cnt[int(c)] = 1
+
+    max_cnt = max(cnt.values())
+    max_num = 0
+
+    for i in range(9, -1, -1):
+        if i in cnt:
+            if cnt[i] >= max_cnt and i >= max_num:
+                max_num = i
+
+    print(f"#{tc} {max_num} {max_cnt}")
