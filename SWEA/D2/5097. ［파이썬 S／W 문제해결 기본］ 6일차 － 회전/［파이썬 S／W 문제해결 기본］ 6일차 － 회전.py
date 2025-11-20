978ee0da5c609f441dc05e4b4ef41e8b26e5ead9
Jimin Ku <82087246+jimizip@users.noreply.github.com>
2025-11-20 23:45:27 +0900

    [D2] Title: [파이썬 S/W 문제해결 기본] 6일차 - 회전, Time: 51 ms, Memory: 52,352 KB -BaekjoonHub

---
+++ SWEA/D2/5097. ［파이썬 S／W 문제해결 기본］ 6일차 － 회전/［파이썬 S／W 문제해결 기본］ 6일차 － 회전.py
@@ -0,0 +1,8 @@
+T = int(input())
+for tc in range(1, T+1):
+    N, M = map(int, input().split())
+    arr = list(map(int, input().split()))
+
+    result = arr[M % N]
+
+    print(f'#{tc} {result}')
