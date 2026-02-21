93b23c310d370499891c68dbfdd6cbc5012744a4
Jimin Ku <82087246+jimizip@users.noreply.github.com>
2026-02-21 18:11:53 +0900

    [Silver III] Title: 소수 구하기, Time: 3120 ms, Memory: 35008 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,14 +1,23 @@
-import math
-
 M, N = map(int, input().split())
-num_list = []
-result = 0
 
-for i in range(M, N+1):
-    if i == 1:
-        continue
-    for j in range(2, int(math.sqrt(i))+1):
-        if i % j == 0:
-            break
-    else:
-        print(i)
+def find_prime_list_under_number(M, N):
+    prime_list = []
+    for n in range(M, N + 1):
+        if n < 2:
+            continue
+        if n == 2:
+            prime_list.append(2)
+            continue
+        if n % 2 == 0:
+            continue
+        for i in range(3, int(n**0.5)+1):
+            if n % i == 0:
+                break
+        else:
+            prime_list.append(n)
+
+    return prime_list
+
+result = find_prime_list_under_number(M, N)
+for i in result:
+    print(i)
