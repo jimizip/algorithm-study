e6ab91fbe3b715b490b2453800735aafdba356d2
Jimin Ku <82087246+jimizip@users.noreply.github.com>
2026-05-11 08:58:42 +0900

    [level 2] Title: 올바른 괄호, Time: 6.94 ms, Memory: 9.01 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,16 @@
+def solution(s):
+    cnt = 0
+    
+    for i in s:
+        if i == '(':
+            cnt += 1
+        elif i == ')':
+            cnt -= 1
+        
+        if cnt < 0:
+            break
+    
+    if cnt == 0:
+        return True
+    else:
+        return False
